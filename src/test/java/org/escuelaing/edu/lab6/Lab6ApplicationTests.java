899d378ca112ab457b88f753d0be6039aca7eb11
Andres Jacobo <andres.sepulveda-s@mail.escuelaing.edu.co>
2025-10-29 12:15:51 -0500

    add:app skeleton

---
+++ src/test/java/org/escuelaing/edu/lab6/Lab6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.escuelaing.edu.lab6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
